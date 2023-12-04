6a3a8f5bad3b109b77300dd144b32e3841ea9b51
DianaRojasUwU <202100168@upqroo.edu.mx>
2023-12-04 10:36:53 -0500

    Cambios en el proyecto

--- babelfrontnend_vue/src/components/Libro/ListarLibro.vue
+++ babelfrontnend_vue/src/components/Libro/ListarLibro.vue
@@ -1,18 +1,19 @@
 <template>
-    <div class="container">
-      <div class="card">
-        <div class="row">
-          <div class="col-md-8">
-            <h5 class="my-2 mr-md-auto font-weight-normal">Libros</h5>
-          </div>
-          <div class="col col-lg-2">
-            <h5 class="my-2 mr-md-auto font-weight-normal">
-              <router-link :to="{ path: '/vistaAdmin/AgregarLibro' }" class="btn btn-warning">Agregar</router-link>
-            </h5>
-          </div>
+  <div class="container">
+    <div class="card">
+      <div class="row">
+        <div class="col-md-8">
+          <h5 class="my-2 mr-md-auto font-weight-normal">Libros</h5>
         </div>
-  
-        <div class="card_body">
+        <div class="col col-lg-2">
+          <h5 class="my-2 mr-md-auto font-weight-normal">
+            <router-link :to="{ path: '/vistaAdmin/AgregarLibro' }" class="btn btn-warning">Agregar</router-link>
+          </h5>
+        </div>
+      </div>
+
+      <div class="card_body">
+        <div class="table-responsive">
           <table class="table table-striped table-bordered">
             <thead>
               <tr>
@@ -29,16 +30,20 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="libro in libros" :key="libro.id">
+              <tr v-for="libro in paginatedLibros" :key="libro.id">
                 <td>{{ libro.id }}</td>
                 <td>{{ libro.titulo }}</td>
                 <td>{{ libro.autor }}</td>
-                <td>{{ libro.descripcion }}</td>
-                <td>{{ libro.imagen }}</td>
+                <td>
+                  <button @click="mostrarModal(libro.descripcion)">Descripción</button>
+                </td>
+                <td>
+                  <button @click="mostrarImagenModal(libro.imagen)">Imagen</button>
+                </td>
                 <td>{{ libro.precio }}</td>
                 <td>{{ libro.stock }}</td>
                 <td>{{ libro.categoria.nombre }}</td>
-  
+
                 <td>
                   <div class="btn-group" role="label" aria-label="">
                     <router-link :to="{ path: '/vistaAdmin/EditarLibro/' + libro.id }" class="btn btn-warning">Editar</router-link>
@@ -53,46 +58,160 @@
             </tbody>
           </table>
         </div>
+
+        <!-- Paginación -->
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
+              <button class="page-link" @click="paginaAnterior">Anterior</button>
+            </li>
+            <li class="page-item" v-for="pageNumber in pages" :key="pageNumber" :class="{ 'active': currentPage === pageNumber }">
+              <button class="page-link" @click="cambiarPagina(pageNumber)">{{ pageNumber }}</button>
+            </li>
+            <li class="page-item" :class="{ 'disabled': currentPage === totalPages }">
+              <button class="page-link" @click="paginaSiguiente">Siguiente</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <!-- Modal para Descripción -->
+      <div v-if="showModal" class="modal">
+        <div class="modal-content">
+          <span @click="cerrarModal" class="close">&times;</span>
+          <p>{{ modalInfo }}</p>
+        </div>
+      </div>
+
+      <!-- Modal para Imagen -->
+      <div v-if="showImagenModal" class="modal">
+        <div class="modal-content">
+          <span @click="cerrarImagenModal" class="close">&times;</span>
+          <p>{{ modalImagen }}</p>
+        </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from "axios";
-  
-  export default {
-    data() {
-      return {
-        libros: [],
-      };
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      libros: [],
+      modalInfo: "",
+      showModal: false,
+      modalImagen: "",
+      showImagenModal: false,
+      itemsPerPage: 5, // Número de elementos por página
+      currentPage: 1, // Página actual
+    };
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.libros.length / this.itemsPerPage);
     },
-    created() {
-      this.consultarLibros();
+    pages() {
+      const pagesArray = [];
+      for (let i = 1; i <= this.totalPages; i++) {
+        pagesArray.push(i);
+      }
+      return pagesArray;
     },
-    methods: {
-      consultarLibros() {
-        axios.get("https://localhost:7102/api/libros")
-          .then((result) => {
-            this.libros = result.data;
-            console.log(result.data);
-          });
-      },
-      borrarLibro(id) {
-        console.log(id);
-  
-        axios.delete("https://localhost:7102/api/libros/" + id)
-          .then(() => {
-            // Redirige después de la eliminación
-            this.consultarLibros();
-          });
-      },
+    paginatedLibros() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.libros.slice(start, end);
     },
-  };
-  </script>
-  
-  <style>
-  span {
-    color: black;
-  }
-  </style>
-  
+  },
+  created() {
+    this.consultarLibros();
+  },
+  methods: {
+    consultarLibros() {
+      axios.get("https://localhost:7102/api/libros")
+        .then((result) => {
+          this.libros = result.data;
+          console.log(result.data);
+        });
+    },
+    borrarLibro(id) {
+      axios.delete("https://localhost:7102/api/libros/" + id)
+        .then(() => {
+          // Redirige después de la eliminación
+          this.consultarLibros();
+        });
+    },
+    mostrarModal(info) {
+      this.modalInfo = info;
+      this.showModal = true;
+    },
+    cerrarModal() {
+      this.showModal = false;
+    },
+    mostrarImagenModal(imagen) {
+      this.modalImagen = imagen;
+      this.showImagenModal = true;
+    },
+    cerrarImagenModal() {
+      this.showImagenModal = false;
+    },
+    cambiarPagina(pageNumber) {
+      this.currentPage = pageNumber;
+    },
+    paginaAnterior() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    paginaSiguiente() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+/* Agrega estilos CSS para el modal según tus necesidades */
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0,0,0,0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+.card_body {
+  overflow-x: auto;
+}
+
+</style>
--- babelfrontnend_vue/src/views/vistaAdmin.vue
+++ babelfrontnend_vue/src/views/vistaAdmin.vue
@@ -12,82 +12,67 @@
   </script>
   
   <style lang="scss">
-  
   .dashboard {
-	display: grid;
-	grid-template-columns: 1fr 5fr;    
-	min-height: 100vh; /* Ajuste mínimo de altura al 100% de la ventana */
-  }
-  
-  .content {
-	background-color: white;
-	border-radius: 10px;
-	margin: 6px 6px 0 6px; /* Elimina el margen inferior */
-	padding: 20px;
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-	overflow-y: auto;
-	height: calc(80% - 10px); /* Ajuste la altura al espacio restante */
-  }
-  @media (max-width: 768px) {
-  .dashboard {
-    grid-template-columns: 1fr; /* Cambia a una columna en dispositivos móviles */
+    display: grid;
+    grid-template-columns: 1fr 5fr;
+    min-height: 100vh;
   }
 
   .content {
+    background-color: white;
+    border-radius: 10px;
     margin: 6px;
-    height: auto; /* Altura automática en dispositivos móviles */
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    overflow-x: auto;
+    height: calc(80% - 10px);
   }
-}
+
+  .table {
+    width: 100%; /* O ajusta el ancho que prefieras */
+    max-width: 100%;
+    table-layout: fixed;
+  }
+
+  .table th, .table td {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+
+  @media (max-width: 768px) {
+    .dashboard {
+      grid-template-columns: 1fr;
+    }
+
+    .content {
+      height: auto;
+    }
+  }
+
   h2 {
-	color: white;
+    color: white;
   }
-  
+
   a {
-	text-decoration: none;
-	color: white;
+    text-decoration: none;
+    color: white;
   }
-  
+
   :root {
-	--primary: #4ade80;
-	--primary-alt: #22c55e;
-	--grey: #64748b;
-	--dark: #1e293b;
-	--dark-alt: #334155;
-	--light: #f1f5f9;
-	--sidebar-width: 300px;
+    --light: #f1f5f9;
   }
-  
-  * {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: 'Fira sans', sans-serif;
-  }
-  
+
   body {
-	background: var(--light);
+    background: var(--light);
   }
-  
+
   button {
-	cursor: pointer;
-	appearance: none;
-	border: none;
-	outline: none;
-	background: none;
+    cursor: pointer;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
   }
-  
-  .app {
-	display: flex;
-  
-	main {
-	  flex: 1 1 0;
-	  padding: 2rem;
-  
-	  @media (max-width: 1024px) {
-		padding-left: 6rem;
-	  }
-	}
-  }
-  
-  </style>
-  
+</style>
